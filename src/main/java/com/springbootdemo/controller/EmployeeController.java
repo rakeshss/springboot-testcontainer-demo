9ad2d35cd351386da0aa29ccc81e803e3bf881a8
Rakesh <rakesh@Rakeshs-MacBook-Pro-2.local>
2022-09-08 16:54:37 +0530

    add controller

---
+++ src/main/java/com/springbootdemo/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.springbootdemo.controller;
+
+import com.springbootdemo.model.Employee;
+import com.springbootdemo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
