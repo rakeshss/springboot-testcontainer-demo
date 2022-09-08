d8dc4f157ccb7b7bbec8077a15c1295cc72e7a6c
Rakesh <rakesh@Rakeshs-MacBook-Pro-2.local>
2022-09-08 16:52:06 +0530

    add model

---
+++ src/main/java/com/springbootdemo/model/Employee.java
@@ -0,0 +1,28 @@
+package com.springbootdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+}
